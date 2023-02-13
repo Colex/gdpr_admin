fdfe2287d1c9d25460c8aaa9158419f161f219ab
Colex <hello@alexcsantos.com>
2023-02-14 04:02:32 +0800

    test: add tests to acts_as_tenant adapter

---
+++ spec/gdpr_admin/tenant_adapters/acts_as_tenant_adapter_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe GdprAdmin::TenantAdapters::ActsAsTenantAdapter do
+  subject(:adapter) { described_class.new }
+
+  describe '#with_tenant' do
+    let(:tenant) { double(:tenant) }
+    let(:block) { -> { :block } }
+
+    it 'calls ActsAsTenant.with_tenant' do
+      allow(ActsAsTenant).to receive(:with_tenant)
+      adapter.with_tenant(tenant, &block)
+      expect(ActsAsTenant).to have_received(:with_tenant).with(tenant, &block)
+    end
+  end
+end
