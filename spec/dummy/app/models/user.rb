4a96f07ca606571c290816963480dc5c1fde743e
Colex <hello@alexcsantos.com>
2023-02-14 02:10:53 +0800

    fix: change process of storing password and password confirmation

--- spec/dummy/app/models/user.rb
+++ spec/dummy/app/models/user.rb
@@ -3,21 +3,27 @@
 class User < ApplicationRecord
   acts_as_tenant(:organization)
 
-  attribute :password, :string
-  attribute :password_confirmation, :string
+  attr_reader :password, :password_confirmation
 
   belongs_to :organization
   has_many :activity_logs
 
   validate :validate_password!
 
+  def password=(value)
+    @password = value
+    self.password_digest = Digest::SHA256.hexdigest(password)
+  end
+
+  def password_confirmation=(value)
+    @password_confirmation = value
+  end
+
   private
 
   def validate_password!
     return if password.blank? || password_confirmation.blank?
-    return errors.add(:password, :confirmation) unless password == password_confirmation
 
-    # Note: this is not meant to be a secure way to store passwords.
-    password_digest = Digest::SHA256.hexdigest(password)
+    errors.add(:password, :confirmation) unless password == password_confirmation
   end
 end
