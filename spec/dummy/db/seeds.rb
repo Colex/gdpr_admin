7f448142c58bb8a4c4e609bdd1032aaadb38c383
Colex <hello@alexcsantos.com>
2023-02-14 02:19:18 +0800

    fix: misconfigured foreign keys in fixtures

--- spec/dummy/db/seeds.rb
+++ spec/dummy/db/seeds.rb
@@ -3,7 +3,7 @@ require 'active_record/fixtures'
 fixtures_dir = Rails.root.join('db', 'fixtures')
 
 fixtures = Dir["#{fixtures_dir}/**/*.yml"].map do |file|
-  File.basename(file, '.yml')
+  file.sub("#{fixtures_dir}/", '').sub('.yml', '')
 end
 
 ActiveRecord::FixtureSet.create_fixtures(fixtures_dir, fixtures)
